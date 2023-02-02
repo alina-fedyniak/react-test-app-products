015fee48acf7bf16b8e6e13b50722772c462c8a7
alina.fedyniak <a.fedynyak@artjoker.team>
2023-02-02 19:17:11 +0200

    add img, functional change colors

--- src/components/ProductContent/ProductContent.jsx
+++ src/components/ProductContent/ProductContent.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import { Image } from 'antd';
+import React, {useEffect, useState, useCallback} from 'react';
+import { Image, Space, Button } from 'antd';
 import { useParams } from 'react-router-dom';
 import {
     StyledWrap,
@@ -14,30 +14,50 @@ import {
 import {getProduct} from "../../services/api";
 
 const ProductContent = () => {
-    const [product, setProduct] = useState([]);
     const { id } = useParams();
-    const productData = product?.colors?.shift();
-    const image = productData?.images?.shift();
+    const [productData, setProductData] = useState([]);
+    const [product, setProduct] = useState(null);
+    const productByColor = product ?? productData?.colors?.at(0);
+    const image = productByColor?.images?.at(0);
+
+    const changeColor = useCallback(
+        (id) => {
+            const product = productData?.colors?.find((item) => item.id === id);
+            setProduct(product)
+        },
+        [setProduct, productData]
+    );
 
     useEffect(() => {
-        getProduct(+id).then((product) => {
-            setProduct(product);
+        getProduct(+id).then((response) => {
+            setProductData(response);
         })
     }, []);
 
     return (
         <>
             {
-                productData && (
+                productByColor && (
                     <StyledWrap>
                         <StyledImg>
                             <Image src={image}/>
                         </StyledImg>
                         <StyledBlockInfo>
-                            <StyledName>{product?.name}</StyledName>
-                            <StyledDescription>{productData?.description}</StyledDescription>
-                            <StyledPrice>{productData?.price}</StyledPrice>
-                            <StyledColorName>{productData?.name}</StyledColorName>
+                            <StyledName>{productData?.name}</StyledName>
+                            <StyledDescription>{productByColor?.description}</StyledDescription>
+                            <StyledPrice>{productByColor?.price}</StyledPrice>
+                            <StyledColorName>{productByColor?.name}</StyledColorName>
+                            <Space direction="vertical">
+                                <Space wrap>
+                                    {productData?.colors &&
+                                        productData?.colors?.map((item, index) => {
+                                            return <Button onClick={() => changeColor(item.id)} key={index} size={'large'}>
+                                                {item.name}
+                                            </Button>
+                                        })
+                                    }
+                                </Space>
+                            </Space>
                             <StyledSizes>sizes</StyledSizes>
                         </StyledBlockInfo>
                     </StyledWrap>
